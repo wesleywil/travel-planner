0dd7bf7eb2452db0bdae9a64d51ef07a10c3800c
wesleywil <wesleywilsonti@gmail.com>
2023-10-22 15:44:22 -0300

    added User model and updated admin.py to include the new Model in the Admin panel

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profiles/')
