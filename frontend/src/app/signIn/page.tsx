3c374f4ac7d52b4f3ad5c210762afacbd8412510
wesleywil <wesleywilsonti@gmail.com>
2023-10-25 21:47:10 -0300

    Implementeded registration and login functionality on signIn and signUp pages.

--- frontend/src/app/signIn/page.tsx
+++ frontend/src/app/signIn/page.tsx
@@ -1,17 +1,59 @@
-export default function Register() {
+"use client";
+
+import { useState } from "react";
+
+export default function SignIn() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8000/api/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data: { expiry: string; token: string } = await response.json();
+      if (response.ok) {
+        console.log("Registration successfull");
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("tokenExpiry", data.expiry);
+      } else {
+        console.log("Registration failed");
+      }
+    } catch (error) {
+      console.error("An error occurred: ", error);
+    }
+  };
   return (
     <main className="min-h-screen min-w-screen flex flex-col items-center justify-center text-xl">
       <h1 className="my-2 text-3xl font-bold">Sign In</h1>
-      <form className="p-2 flex flex-col gap-2 text-black">
+      <form
+        onSubmit={handleLogin}
+        className="p-2 flex flex-col gap-2 text-black"
+      >
         <input
           type="text"
           placeholder="Username"
           className="px-2 py-1 rounded"
+          value={formData.username}
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.target.value })
+          }
         />
         <input
           type="password"
           placeholder="Password"
           className="px-2 py-1 rounded"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
         />
 
         <div className="mt-2 flex gap-4 justify-center">
--- frontend/src/app/signUp/page.tsx
+++ frontend/src/app/signUp/page.tsx
@@ -1,36 +1,98 @@
-export default function Register() {
+"use client";
+
+import { useState } from "react";
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [passwordMath, setPasswordMatch] = useState(true);
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (formData.password === formData.confirmPassword) {
+      try {
+        const response = await fetch("http://localhost:8000/api/register/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          console.log("Registration successfull");
+        } else {
+          console.log("Registration failed");
+        }
+      } catch (error) {
+        console.error("An error occurred: ", error);
+      }
+    }
+    // Passwords don't match, set a flag to indicate this to the user.
+    setPasswordMatch(false);
+  };
   return (
     <main className="min-h-screen min-w-screen flex flex-col items-center justify-center text-xl">
       <h1 className="my-2 text-3xl font-bold">Register</h1>
-      <form className="p-2 flex flex-col gap-2 text-black">
+      <form
+        onSubmit={handleRegister}
+        className="p-2 flex flex-col gap-2 text-black"
+      >
         <input
           type="text"
           placeholder="Username"
           className="px-2 py-1 rounded"
+          value={formData.username}
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.target.value })
+          }
         />
         <input
           type="email"
           placeholder="E-mail"
           className="px-2 py-1 rounded"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
         />
         <input
           type="password"
           placeholder="Password"
           className="px-2 py-1 rounded"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
         />
         <input
           type="password"
           placeholder="Confirm Password"
           className="px-2 py-1"
+          value={formData.confirmPassword}
+          onChange={(e) =>
+            setFormData({ ...formData, confirmPassword: e.target.value })
+          }
         />
+        <div className="w-96 px-2 text-center">
+          {!passwordMath ? (
+            <h1 className="text-red-400 text-base">
+              Password does not match. Please try again!
+            </h1>
+          ) : (
+            ""
+          )}
+        </div>
+
         <div className="mt-2 flex gap-4 justify-center">
+          <button className="px-2 py-1 bg-black hover:bg-gray-700 border border-white text-white rounded">
+            Register
+          </button>
           <button
             type="button"
             className="px-2 py-1 bg-black hover:bg-gray-700 border border-white text-white rounded"
           >
-            Register
-          </button>
-          <button className="px-2 py-1 bg-black hover:bg-gray-700 border border-white text-white rounded">
             Cancel
           </button>
         </div>
