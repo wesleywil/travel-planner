9212fd34faf7e96d3536fc19d1cd313406349aeb
wesleywil <wesleywilsonti@gmail.com>
2023-10-27 17:13:42 -0300

    updated profile page and profile_user_information component by using the backend api's to display the user information

--- frontend/src/components/profile_user_information/profile_user_information.component.tsx
+++ frontend/src/components/profile_user_information/profile_user_information.component.tsx
@@ -1,4 +1,12 @@
-const ProfileUserInformation = () => {
+type ProfileUserInformationProps = {
+  username: string;
+  email: string;
+};
+
+const ProfileUserInformation = ({
+  username,
+  email,
+}: ProfileUserInformationProps) => {
   return (
     <div className="flex flex-col items-center">
       <img
@@ -7,10 +15,10 @@ const ProfileUserInformation = () => {
         className="w-32 h-32 p-2 rounded-full"
       />
       <h1 className="w-48 px-2 break-words text-center text-slate-200">
-        Username
+        {username}
       </h1>
       <h2 className="w-48 px-2 break-words text-xs text-center text-slate-200">
-        Email@mail.com
+        {email}
       </h2>
     </div>
   );
