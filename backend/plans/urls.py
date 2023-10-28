874c065a7ddd5ff21acff9029c3a87ebf193f27f
wesleywil <wesleywilsonti@gmail.com>
2023-10-28 15:50:27 -0300

    removed some unecessary backslash's and fixed the urls of the toDo view classes

--- backend/plans/urls.py
+++ backend/plans/urls.py
@@ -11,7 +11,7 @@ app_name = "plans_apis"
 urlpatterns = [
     # Crud plans and toDo
     path('', PlansViewSet.as_view(), name='plans'),
-    path('/<int:pk>/', PlansDetailsViewSet.as_view(), name="plans_details"),
-    path('', ToDoViewSet.as_view(), name='toDo'),
-    path('/<int:pk>/', ToDoDetailsViewSet.as_view(), name="toDo_details"),
+    path('<int:pk>/', PlansDetailsViewSet.as_view(), name="plans_details"),
+    path('toDo/', ToDoViewSet.as_view(), name='toDo'),
+    path('toDo/<int:pk>/', ToDoDetailsViewSet.as_view(), name="toDo_details"),
 ]
