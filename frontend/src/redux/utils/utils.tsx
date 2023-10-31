125b010bcebac21f549aabe149dc1dc24d2f76bc
wesleywil <wesleywilsonti@gmail.com>
2023-10-31 17:40:39 -0300

    updated utilsReducer by including a new state and reducer function to deal with the plans details panel

--- frontend/src/redux/utils/utils.tsx
+++ frontend/src/redux/utils/utils.tsx
@@ -2,10 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface UtilState {
   hideFormPlan: boolean;
+  hidePlanDetails: boolean;
 }
 
 const initialState: UtilState = {
   hideFormPlan: true,
+  hidePlanDetails: true,
 };
 
 export const utilsSlice = createSlice({
@@ -15,9 +17,12 @@ export const utilsSlice = createSlice({
     switchFormHidden: (state) => {
       state.hideFormPlan = !state.hideFormPlan;
     },
+    switchPlanDetailsHidden: (state) => {
+      state.hidePlanDetails = !state.hidePlanDetails;
+    },
   },
 });
 
-export const { switchFormHidden } = utilsSlice.actions;
+export const { switchFormHidden, switchPlanDetailsHidden } = utilsSlice.actions;
 
 export default utilsSlice.reducer;
