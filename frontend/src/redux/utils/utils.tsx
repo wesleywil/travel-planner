59c5b502932ee2fa6fb839afa097506cc507aeaa
wesleywil <wesleywilsonti@gmail.com>
2023-11-03 13:05:36 -0300

    updated utilsSlicer by including a new reducer function and state to handle todoDetails

--- frontend/src/redux/utils/utils.tsx
+++ frontend/src/redux/utils/utils.tsx
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 export interface UtilState {
   hideFormPlan: boolean;
   hidePlanDetails: boolean;
+  hideTodoDetails: boolean;
 }
 
 const initialState: UtilState = {
   hideFormPlan: true,
   hidePlanDetails: true,
+  hideTodoDetails: true,
 };
 
 export const utilsSlice = createSlice({
@@ -20,9 +22,16 @@ export const utilsSlice = createSlice({
     switchPlanDetailsHidden: (state) => {
       state.hidePlanDetails = !state.hidePlanDetails;
     },
+    switchTodoDetailsHidden: (state) => {
+      state.hideTodoDetails = !state.hideTodoDetails;
+    },
   },
 });
 
-export const { switchFormHidden, switchPlanDetailsHidden } = utilsSlice.actions;
+export const {
+  switchFormHidden,
+  switchPlanDetailsHidden,
+  switchTodoDetailsHidden,
+} = utilsSlice.actions;
 
 export default utilsSlice.reducer;
